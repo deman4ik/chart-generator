bfe5352eecfb863650c855dacc8060c63dc3cbe3
Pavel Dotsenko <pavel_dotsenko@hotmail.com>
2019-12-23 17:01:47 +0600

    summFormat

--- data.js
+++ data.js
@@ -1,28 +1,96 @@
-const positions = {
+const robot = {
+  "id": "f1893b3c-8768-40d7-a3c0-a438f994f6f8",
   "name": "BR-1 Bitfinex BTC/USD 1d",
-  "robotPositions": [
-    {
-      "code": "p_68",
-      "entryPrice": 8982.1,
-      "entryDate": "2019-11-08T00:00:00",
-      "entryAction": "short",
-      "exitPrice": 7414.4,
-      "exitDate": "2019-11-27T16:49:06",
-      "exitAction": "closeShort"
-    }
-  ]
-}
+  "exchange": "bitfinex",
+  "asset": "BTC",
+  "currency": "USD",
+  "timeframe": 1440
+};
+
+const position = {
+           "code": "p_68",
+           "status": "closed",
+           "entryPrice": 8982.1,
+           "entryDate": "2019-11-08T09:11:16.28",
+           "entryCandleTimestamp": "2019-11-08T00:00:00",
+           "entryAction": "short",
+           "exitPrice": 7414.4,
+           "exitDate": "2019-11-27T16:49:06.92",
+           "exitCandleTimestamp": "2019-11-27T00:00:00",
+           "exitAction": "closeShort",
+           "profit": 1567.7
+};
 
 const candles = [
   {
-    "id": "d58f82f3-1461-42fe-a454-d9fb7724f89d",
-    "time": 1572739200000,
-    "timestamp": "2019-11-03T00:00:00",
-    "open": 9332.2,
-    "high": 9400.6,
-    "low": 9100,
-    "close": 9222.4,
-    "volume": 3616.53932
+    "id": "2e0be58c-7b36-496c-a079-0341aa33d0d7",
+    "time": 1573689600000,
+    "timestamp": "2019-11-14T00:00:00",
+    "open": 8800.993697,
+    "high": 8817.4,
+    "low": 8600,
+    "close": 8655.7,
+    "volume": 3304.675936
+  },
+  {
+    "id": "e7884be9-d6cc-4712-a099-03d47be3090b",
+    "time": 1573862400000,
+    "timestamp": "2019-11-16T00:00:00",
+    "open": 8495.8,
+    "high": 8577,
+    "low": 8440.1,
+    "close": 8523.5,
+    "volume": 1534.610947
+  },
+  {
+    "id": "8f7d23b2-da0b-4a46-9174-19b7f2ea0ea7",
+    "time": 1573948800000,
+    "timestamp": "2019-11-17T00:00:00",
+    "open": 8523.4,
+    "high": 8699.5,
+    "low": 8404.4,
+    "close": 8537.3,
+    "volume": 2837.153673
+  },
+  {
+    "id": "e44891f1-8db9-43a2-90df-60199945df7d",
+    "time": 1574035200000,
+    "timestamp": "2019-11-18T00:00:00",
+    "open": 8537,
+    "high": 8537,
+    "low": 8117,
+    "close": 8219.5,
+    "volume": 6362.436607
+  },
+  {
+    "id": "2cf6a10a-d938-41c4-acd2-9c2eb13cf4aa",
+    "time": 1574294400000,
+    "timestamp": "2019-11-21T00:00:00",
+    "open": 8126.4,
+    "high": 8155.9,
+    "low": 7500,
+    "close": 7671.1,
+    "volume": 9890.619938
+  },
+  {
+    "id": "4925ef4e-c1f2-4559-9354-aa1b7fb74ac3",
+    "time": 1574380800000,
+    "timestamp": "2019-11-22T00:00:00",
+    "open": 7671.1,
+    "high": 7766,
+    "low": 6819.2,
+    "close": 7317.2,
+    "volume": 19368.415301
+  },
+  {
+    "id": "04f06152-234e-4670-ad9f-20b1f653f4f9",
+    "time": 1574467200000,
+    "timestamp": "2019-11-23T00:00:00",
+    "open": 7318.6,
+    "high": 7385,
+    "low": 7141,
+    "close": 7352.6,
+    "volume": 4626.890504
   },
   {
     "id": "84bc9b9d-aa66-4fef-a462-88715c2631cb",
@@ -66,13 +134,23 @@ const candles = [
   },
   {
     "id": "388dcee6-ee4d-437a-8fe2-ae3e8f64af1d",
-    "time": 1573149600000,
+    "time": 1573171200000,
     "timestamp": "2019-11-08T00:00:00",
     "open": 9212.5,
     "high": 9252.9,
     "low": 8681,
     "close": 8782.3,
     "volume": 7864.404383
+  },
+  {
+    "id": "c354d316-147d-4884-9c67-6b60a1aa54ad",
+    "time": 1573516800000,
+    "timestamp": "2019-11-12T00:00:00",
+    "open": 8737.715159,
+    "high": 8910,
+    "low": 8602,
+    "close": 8830,
+    "volume": 3504.663003
   },
   {
     "id": "0ba2db8b-f3be-46b1-8f46-adad8805d637",
@@ -105,16 +183,6 @@ const candles = [
     "volume": 4243.05004
   },
   {
-    "id": "c354d316-147d-4884-9c67-6b60a1aa54ad",
-    "time": 1573516800000,
-    "timestamp": "2019-11-12T00:00:00",
-    "open": 8737.715159,
-    "high": 8910,
-    "low": 8602,
-    "close": 8830,
-    "volume": 3504.663003
-  },
-  {
     "id": "dfe41e1b-9c2b-442c-8a9a-ef87853a0bec",
     "time": 1573603200000,
     "timestamp": "2019-11-13T00:00:00",
@@ -125,14 +193,14 @@ const candles = [
     "volume": 1340.100282
   },
   {
-    "id": "2e0be58c-7b36-496c-a079-0341aa33d0d7",
-    "time": 1573689600000,
-    "timestamp": "2019-11-14T00:00:00",
-    "open": 8800.993697,
-    "high": 8817.4,
-    "low": 8600,
-    "close": 8655.7,
-    "volume": 3304.675936
+    "id": "d58f82f3-1461-42fe-a454-d9fb7724f89d",
+    "time": 1572739200000,
+    "timestamp": "2019-11-03T00:00:00",
+    "open": 9332.2,
+    "high": 9400.6,
+    "low": 9100,
+    "close": 9222.4,
+    "volume": 3616.53932
   },
   {
     "id": "5dcd10f7-dba2-40f6-9300-d9ab216b5b22",
@@ -143,36 +211,6 @@ const candles = [
     "low": 8420.1,
     "close": 8495.832625,
     "volume": 5184.166755
-  },
-  {
-    "id": "e7884be9-d6cc-4712-a099-03d47be3090b",
-    "time": 1573862400000,
-    "timestamp": "2019-11-16T00:00:00",
-    "open": 8495.8,
-    "high": 8577,
-    "low": 8440.1,
-    "close": 8523.5,
-    "volume": 1534.610947
-  },
-  {
-    "id": "8f7d23b2-da0b-4a46-9174-19b7f2ea0ea7",
-    "time": 1573948800000,
-    "timestamp": "2019-11-17T00:00:00",
-    "open": 8523.4,
-    "high": 8699.5,
-    "low": 8404.4,
-    "close": 8537.3,
-    "volume": 2837.153673
-  },
-  {
-    "id": "e44891f1-8db9-43a2-90df-60199945df7d",
-    "time": 1574035200000,
-    "timestamp": "2019-11-18T00:00:00",
-    "open": 8537,
-    "high": 8537,
-    "low": 8117,
-    "close": 8219.5,
-    "volume": 6362.436607
   },
   {
     "id": "246938ba-ced8-45bc-9e80-929fd14be7e7",
@@ -193,36 +231,6 @@ const candles = [
     "low": 8081,
     "close": 8126.4,
     "volume": 2060.185832
-  },
-  {
-    "id": "2cf6a10a-d938-41c4-acd2-9c2eb13cf4aa",
-    "time": 1574294400000,
-    "timestamp": "2019-11-21T00:00:00",
-    "open": 8126.4,
-    "high": 8155.9,
-    "low": 7500,
-    "close": 7671.1,
-    "volume": 9890.619938
-  },
-  {
-    "id": "4925ef4e-c1f2-4559-9354-aa1b7fb74ac3",
-    "time": 1574380800000,
-    "timestamp": "2019-11-22T00:00:00",
-    "open": 7671.1,
-    "high": 7766,
-    "low": 6819.2,
-    "close": 7317.2,
-    "volume": 19368.415301
-  },
-  {
-    "id": "04f06152-234e-4670-ad9f-20b1f653f4f9",
-    "time": 1574467200000,
-    "timestamp": "2019-11-23T00:00:00",
-    "open": 7318.6,
-    "high": 7385,
-    "low": 7141,
-    "close": 7352.6,
-    "volume": 4626.890504
   },
   {
     "id": "0e096bbb-212a-4d82-ac78-598e4140677a",
@@ -252,7 +260,7 @@ const candles = [
     "high": 7386.3,
     "low": 7047.2,
     "close": 7198.3,
-    "volume": 5195.343918000134
+    "volume": 5197.479427
   },
   {
     "id": "f12500d2-2d2a-496b-b28c-825f477e5613",
@@ -262,8 +270,8 @@ const candles = [
     "high": 7689,
     "low": 6878.2,
     "close": 7549.3,
-    "volume": 13686.434843999237
+    "volume": 13895.627007
   }
 ];
 
-module.exports = { positions, candles };
+module.exports = { robot, position, candles };
--- index.js
+++ index.js
@@ -1,535 +1,109 @@
-const ChartjsNode = require('chartjs-node');
+const { generateImage } = require("./chartGenerator");
 
-var chartNode = new ChartjsNode(1200, 700);
-
-const { positions, candles } = require('./data')
-
-const ChartTitle = positions["name"]
-
-const chartData = [];
-const pointData = [];
-
-dataSet(candles);
-setPoints(positions["robotPositions"]);
-
-const months = {
-    '01': 'Jan',
-    '02': 'Feb',
-    '03': 'Mar',
-    '04': 'Apr',
-    '05': 'May',
-    '06': 'Jun',
-    '07': 'Jul',
-    '08': 'Aug',
-    '09': 'Sep',
-    '10': 'Oct',
-    '11': 'Nov',
-    '12': 'Dec'
-}
-
-const typeActions = {
-    'short': 'Short',
-    'long': 'Long',
-    'closeShort': 'Close Short',
-    'closeLong': 'Close Long'
-}
-
-chartNode.on('beforeDraw', function (Chart) {
-    'use strict';
-
-    Chart = Chart && Chart.hasOwnProperty('default') ? Chart['default'] : Chart;
-
-    const helpers = Chart.helpers;
-	const defaultConfig = {
-		position: 'left',
-		ticks: {
-			callback: Chart.Ticks.formatters.linear
-		}
-    };
-    
-    Chart.plugins.register({
-		beforeDraw: function(chartInstance) {
-			const ctx = chartInstance.chart.ctx;
-
-		  	ctx.fillStyle = "#242B4A";
-		  	ctx.fillRect(0, 0, chartInstance.chart.width, chartInstance.chart.height);
-		}
-    });
-    
-    const FinancialLinearScale = Chart.scaleService.getScaleConstructor('linear').extend({
-		_parseValue: function (value) {
-            let start, end, min, max;
-
-			if (typeof value.c !== 'undefined') {
-				start = +this.getRightValue(value.l);
-				end   = +this.getRightValue(value.h);
-				min   = Math.min(start, end);
-				max   = Math.max(start, end);
-			} else {
-				value = +this.getRightValue(value.y);
-				start = undefined;
-				end   = value;
-				min   = value;
-				max   = value;
-			}
-
-			return {
-				min: min,
-				max: max,
-				start: start,
-				end: end
-			};
-		},
-		determineDataLimits: function() {
-			const me    = this;
-			const chart = me.chart;
-			const data  = chart.data;
-			const datasets = data.datasets;
-            const isHorizontal = me.isHorizontal();
-
-			function IDMatches(meta) {
-				return isHorizontal ? meta.xAxisID === me.id : meta.yAxisID === me.id;
-			}
-
-			me.min = null;
-			me.max = null;
-
-			helpers.each(datasets, function(dataset, datasetIndex) {
-				const meta = chart.getDatasetMeta(datasetIndex);
-				
-				if (chart.isDatasetVisible(datasetIndex) && IDMatches(meta)) {
-					helpers.each(dataset.data, function(rawValue, index) {
-                        const value = me._parseValue(rawValue);
-
-						if (isNaN(value.min) || isNaN(value.max) || meta.data[index].hidden) return;
-						if (me.min === null || value.min < me.min) me.min = value.min;
-						if (me.max === null || me.max < value.max) me.max = value.max;
-					});
-				}
-			});
-
-			const space = (me.max - me.min) * 0.2;
-
-			me.min -= space;
-            me.max += space;
-
-			this.handleTickRangeOptions();
-		}
-    });
-    
-    Chart.scaleService.registerScaleType('financialLinear', FinancialLinearScale, defaultConfig);
-
-    Chart.defaults.financial = {
-		label: '',
-		hover: { mode: 'label' },
-		scales: {
-			xAxes: [{
-				type: 'time',
-				distribution: 'series',
-				offset: true,
-				ticks: {
-					major: {
-						enabled: true,
-						fontStyle: 'bold'
-                    },
-                    fontSize: 18,
-                    fontStyle: 'bold',
-                    fontColor: '#ffffff',
-					source: 'data',
-					maxRotation: 0,
-					autoSkip: true,
-					autoSkipPadding: 25,
-					sampleSize: 100
-				}
-			}],
-            yAxes: [{
-                type: 'financialLinear',
-                distribution: 'series',
-				offset: true,
-                ticks: {
-					major: {
-						enabled: true,
-						fontStyle: 'bold'
-                    },
-                    fontSize: 18,
-                    fontStyle: 'bold',
-                    fontColor: '#ffffff',
-					source: 'data',
-					maxRotation: 0,
-					autoSkip: true,
-					autoSkipPadding: 25,
-					sampleSize: 100
-				}
-            }]
-		}
-    };
-    
-    const FinancialController = Chart.controllers.bar.extend({
-		dataElementType: Chart.elements.Financial,
-
-		/**
-		 * @private
-		 */
-		_updateElementGeometry: function(element, index, reset, options) {
-			const me    = this;
-			const model = element._model;
-			
-
-            const vscale = me._getValueScale();
-            const base   = vscale.getBasePixel();
-			const ruler  = me._ruler || me.getRuler();
-            const horizontal = vscale.isHorizontal();
-			const vpixels    = me.calculateBarValuePixels(me.index, index, options);
-			const ipixels    = me.calculateBarIndexPixels(me.index, index, ruler, options);
-			const chart      = me.chart;
-			const datasets   = chart.data.datasets;
-            const indexData = datasets[me.index].data[index];
-
-			model.horizontal = horizontal;
-			model.base = reset ? base : vpixels.base;
-			model.x = horizontal ? reset ? base : vpixels.head : ipixels.center;
-			model.y = horizontal ? ipixels.center : reset ? base : vpixels.head;
-			model.height = horizontal ? ipixels.size : undefined;
-			model.width  = horizontal ? undefined : ipixels.size;
-			model.candleOpen  = vscale.getPixelForValue(Number(indexData.o));
-			model.candleHigh  = vscale.getPixelForValue(Number(indexData.h));
-			model.candleLow   = vscale.getPixelForValue(Number(indexData.l));
-			model.candleClose = vscale.getPixelForValue(Number(indexData.c));
-			model.t_data = me._data[index].t
-		},
-
-		draw: function() {
-			const ctx     = this.chart.chart.ctx;
-			const elems   = this.getMeta().data;
-			const dataset = this.getDataset();
-
-			Chart.canvasHelpers.clipArea(ctx, this.chart.chartArea);
-
-			for (let i = 0; i < elems.length; i++) {
-                const d = dataset.data[i].o;
-
-				if (d !== null && d !== undefined && !isNaN(d)) elems[i].draw();
-			}
-
-			Chart.canvasHelpers.unclipArea(ctx);
-		}
-    });
-    
-    const globalOpts = Chart.defaults.global;
-
-	globalOpts.elements.financial = {
-		color: {
-			up: '#1CA46B',
-			down: '#CD3E60',
-			unchanged: '#999'
-		}
-    };
-    
-    /**
-	 * Helper function to get the bounds of the candle
-	 * @private
-	 * @param bar {Chart.Element.financial} the bar
-	 * @return {Bounds} bounds of the bar
-	 */
-	function getBarBounds(candle) {
-		const vm = candle._view;
-		const halfWidth = vm.width / 2;
-
-		let x1 = vm.x - halfWidth,
-			x2 = vm.x + halfWidth,
-			y1 = vm.candleHigh,
-			y2 = vm.candleLow;
-
-		return {
-			left: x1,
-			top: y1,
-			right: x2,
-			bottom: y2
-		};
-    }
-    
-    const FinancialElement = Chart.Element.extend({ inRange: () => {} });
-
-	const helpers$3    = Chart.helpers;
-	const globalOpts$1 = Chart.defaults.global;
-
-	globalOpts$1.elements.candlestick = helpers$3.merge({}, [globalOpts$1.elements.financial, {
-		borderColor: globalOpts$1.elements.financial.color.unchanged,
-		borderWidth: 2,
-	}]);
-
-	const CandlestickElement = FinancialElement.extend({
-		draw: function() {
-			const ctx = this._chart.ctx;
-			const vm = this._view;
-
-			const x = vm.x;
-			const o = vm.candleOpen;
-			const h = vm.candleHigh;
-			const l = vm.candleLow;
-			const c = vm.candleClose;
-			const t = vm.t_data;
-
-			let borderColors = vm.borderColor;
-
-			if (typeof borderColors === 'string') {
-				borderColors = {
-					up: borderColors,
-					down: borderColors,
-					unchanged: borderColors
-				};
-			}
-
-			let borderColor;
-
-			if (c < o) {
-				borderColor = helpers$3.getValueOrDefault(borderColors ? borderColors.up : undefined, globalOpts$1.elements.candlestick.color.up);
-				ctx.fillStyle = helpers$3.getValueOrDefault(vm.color ? vm.color.up : undefined, globalOpts$1.elements.candlestick.color.up);
-			} else if (c > o) {
-				borderColor = helpers$3.getValueOrDefault(borderColors ? borderColors.down : undefined, globalOpts$1.elements.candlestick.color.down);
-				ctx.fillStyle = helpers$3.getValueOrDefault(vm.color ? vm.color.down : undefined, globalOpts$1.elements.candlestick.color.down);
-			} else {
-				borderColor = helpers$3.getValueOrDefault(borderColors ? borderColors.unchanged : undefined, globalOpts$1.elements.candlestick.color.unchanged);
-				ctx.fillStyle = helpers$3.getValueOrDefault(vm.color ? vm.color.unchanged : undefined, globalOpts$1.elements.candlestick.color.unchanged);
-			}
-
-			ctx.lineWidth = helpers$3.getValueOrDefault(vm.borderWidth, globalOpts$1.elements.candlestick.borderWidth);
-			ctx.strokeStyle = helpers$3.getValueOrDefault(borderColor, globalOpts$1.elements.candlestick.borderColor);
-
-			ctx.beginPath();
-			ctx.moveTo(x, h);
-			ctx.lineTo(x, Math.min(o, c));
-			ctx.moveTo(x, l);
-			ctx.lineTo(x, Math.max(o, c));
-			ctx.stroke();
-			ctx.fillRect(x - vm.width / 6, c, vm.width / 3, o - c);
-            ctx.closePath();
-
-			for (let k = 0; k < pointData.length; k++) if (pointData[k].entry_date_format == t) fillPoin(ctx, {x, h, l, elem: pointData[k]})
-		},
-    });
-    
-    Chart.defaults.candlestick = Chart.helpers.merge({}, Chart.defaults.financial);
-
-	Chart.defaults._set('global', {
-		datasets: {
-			candlestick: Chart.defaults.global.datasets.bar
-		}
-    });
-    
-    Chart.controllers.candlestick = FinancialController.extend({
-		dataElementType: CandlestickElement,
-
-		updateElement: function (element, index, reset) {
-			const meta = this.getMeta();
-			const dataset = this.getDataset();
-			const options = this._resolveDataElementOptions(element, index);
-
-			element._xScale = this.getScaleForId(meta.xAxisID);
-			element._yScale = this.getScaleForId(meta.yAxisID);
-			element._datasetIndex = this.index;
-			element._index = index;
-
-			element._model = {
-				datasetLabel: dataset.label || '',
-				color: dataset.color,
-				borderColor: dataset.borderColor,
-				borderWidth: dataset.borderWidth,
-			};
-
-			this._updateElementGeometry(element, index, reset, options);
-
-			element.pivot();
-		},
-
-	});
-    //Chartjs.defaults
-    //Chartjs.pluginService
-    //Chartjs.scaleService
-    //Chartjs.layoutService
-    //Chartjs.helpers
-    //Chartjs.controllers
-    //etc
-});
- 
-const chartJsOptions = {
-	type: 'candlestick',
-	data: {
-	  	datasets: [{
-			label: ChartTitle,
-			data: chartData,
-			color: {
-				up: '#1CA46B',
-				down: '#CD3E60',
-				unchanged: '#999',
-			}
-		}]
-	},
-	options: {
-	  	layout: {
-			padding: 20,
-			backgroundColor: 'rgba(251, 85, 85, 0.4)',
-	  	},
-		legend: {
-		  	display: true,
-		  	position: 'top',
-		  	padding: 0,
-		  	align: 'start',
-		  	labels: {
-			  	fontSize: 48,
-			  	fontFamily: "'Helvetica Neue', 'Helvetica', 'Arial', sans-serif",
-			  	fontStyle: 'bold',
-			  	fontColor: '#FFFFFF',
-			  	boxWidth: 0
-		  	}
-		},
-		scales: {
-			xAxes: [{
-                fontColor: '#ffffff',
-                fontSize: 16
-            }],
-			yAxes: [{
-                fontColor: '#ffffff',
-                fontSize: 16
-            }]
-		}
-	}
+const robot = {
+	"id": "37dd7d07-99f3-471c-997d-cae0cbe188eb",
+	"name": "PAR-1 Bitfinex XLM/USD 8h",
+	"exchange": "bitfinex",
+	"asset": "XLM",
+	"currency": "USD",
+	"timeframe": 480
 };
 
-function fillPoin(ctx, params) {
-	const y  = (params.l < params.h ? params.l : params.h) - 10,
-		data = params.elem,
-		dateTime = dateTimeFormat(data.entry_date),
-		circleClr = ctx.createLinearGradient(0, 20, 0, 0);
-	
-	let borderClr, firstClr, secondClr
+const position = {
+		"code": "p_15",
+		"status": "open",
+		"entryPrice": 0.057777,
+		"entryDate": "2019-11-22T10:01:51.656",
+		"entryCandleTimestamp": "2019-11-22T08:00:00",
+		"entryAction": "short",
+		"exitPrice": null,
+		"exitDate": null,
+		"exitCandleTimestamp": null,
+		"exitAction": null,
+		"profit": null
+};
 
-	if (data.entry_action == "long") {
-		borderClr = 'rgba(25, 140, 127, 0.2)';
-		firstClr  = "#198C7F";
-		secondClr = "#16B3EA";
-	} else if (data.entry_action == "short") {
-		borderClr = 'rgba(189, 54, 86, 0.2)';
-		firstClr  = "#D63535";
-		secondClr = "#9952E0";
-	} else {
-		borderClr = 'rgba(153, 153, 153, 0.2)';
-		firstClr  = "rgba(153, 153, 153, 0.2)";
-		secondClr = "rgba(153, 153, 153, 0.2)";
-	}
-		
-	ctx.fillStyle = borderClr
-	ctx.beginPath();
-	ctx.arc(params.x, y - 4, 13, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.closePath();
+const candles = [
+  {
+	"id": "fb441329-8639-45f3-999f-fedf0f597eb1",
+	"time": 1574208000000,
+	"timestamp": "2019-11-20T00:00:00",
+	"open": 0.065858,
+	"high": 0.066261,
+	"low": 0.065319,
+	"close": 0.0656,
+	"volume": 124252.22837399998
+  },
+  {
+	"id": "8e24183f-1776-41e3-b32d-eae665399b07",
+	"time": 1574236800000,
+	"timestamp": "2019-11-20T08:00:00",
+	"open": 0.065718,
+	"high": 0.066079,
+	"low": 0.0645,
+	"close": 0.065732,
+	"volume": 104676.886939
+  },
+  {
+	"id": "2f09c24c-a6fb-41b2-8e55-d326c4b1f1d0",
+	"time": 1574265600000,
+	"timestamp": "2019-11-20T16:00:00",
+	"open": 0.064973,
+	"high": 0.065349,
+	"low": 0.063923,
+	"close": 0.063923,
+	"volume": 44876.898754
+  },
+  {
+	"id": "578a732a-5933-4fc8-90cb-2d7215513e0d",
+	"time": 1574294400000,
+	"timestamp": "2019-11-21T00:00:00",
+	"open": 0.063923,
+	"high": 0.063932,
+	"low": 0.062823,
+	"close": 0.062823,
+	"volume": 141870.295216
+  },
+  {
+	"id": "0730bd14-37fc-48b9-88b9-61c2dad48600",
+	"time": 1574323200000,
+	"timestamp": "2019-11-21T08:00:00",
+	"open": 0.062817,
+	"high": 0.062817,
+	"low": 0.057777,
+	"close": 0.060044,
+	"volume": 1286631.102515
+  },
+  {
+	"id": "b1b35266-1074-4f33-b068-a61291975293",
+	"time": 1574352000000,
+	"timestamp": "2019-11-21T16:00:00",
+	"open": 0.060391,
+	"high": 0.061301,
+	"low": 0.060016,
+	"close": 0.060946,
+	"volume": 280054.293645
+  },
+  {
+	"id": "bc9f039f-8bcb-49b7-9203-971ed1599df8",
+	"time": 1574380800000,
+	"timestamp": "2019-11-22T00:00:00",
+	"open": 0.061139,
+	"high": 0.061457,
+	"low": 0.058488,
+	"close": 0.059477,
+	"volume": 160230.214415
+  },
+  {
+	"id": "35c473a8-3a95-47da-855f-abe0b2543910",
+	"time": 1574409600000,
+	"timestamp": "2019-11-22T08:00:00",
+	"open": 0.05975,
+	"high": 0.060004,
+	"low": 0.053315,
+	"close": 0.056293,
+	"volume": 1071620.082256
+  }
+];
 
-	circleClr.addColorStop(0, firstClr);
-	circleClr.addColorStop(1, secondClr);
-
-	ctx.fillStyle = circleClr;
-	ctx.beginPath();
-	ctx.arc(params.x, y - 4, 10, 0, 2 * Math.PI);
-	ctx.fill();
-	ctx.closePath();
-
-	ctx.beginPath();
-
-	if (data.exit_action == "closeLong") {
-		ctx.moveTo(params.x - 6, y - 2);
-		ctx.lineTo(params.x + 6, y - 2);
-		ctx.lineTo(params.x, y - 8);
-	} else if (data.exit_action == "closeShort") {
-		ctx.moveTo(params.x, y);
-		ctx.lineTo(params.x - 6, y - 6);
-		ctx.lineTo(params.x + 6, y - 6);
-	}
-
-	ctx.fillStyle = "#ffffff";
-	ctx.fill();
-	ctx.closePath();
-
-	ctx.beginPath();
-	ctx.moveTo(params.x, y - 14);
-	ctx.lineTo(params.x - 4, y - 20);
-	ctx.arcTo(params.x - 80, y - 20, params.x - 80, y - 28, 4);
-	ctx.arcTo(params.x - 80, y - 120, params.x - 72, y - 120, 4);
-	ctx.arcTo(params.x + 80, y - 120, params.x + 80, y - 116, 4);
-	ctx.arcTo(params.x + 80, y - 20, params.x + 72, y - 20, 4);
-	ctx.lineTo(params.x + 4, y - 20);
-	ctx,lineWidth = 1
-	ctx.fillStyle = "#3D4977";
-	ctx.strokeStyle = "#39436A";
-	ctx.stroke();
-	ctx.fill();
-    ctx.closePath();
-    
-	ctx.font = "bold 24px 'Helvetica Neue', 'Helvetica', 'Arial', 'sans-serif'";
-	ctx.fillStyle = "#ffffff"; // "#AEC6EE"
-	ctx.textAlign = "center";
-	ctx.fillText(dateTime, params.x, y - 40);
-	ctx.fillText(summFormat(data.entry_price), params.x, y - 70);
-	ctx.fillText(typeActions[data.entry_action] + ": " + data.code, params.x, y - 100);
-	ctx.globalCompositeOperation = "source-over";
-}
-
-function summFormat(params) {
-    let nums = ((params + "").split("").reverse().join("").replace(/(\d{3})/g, "$1,").split("").reverse().join("").replace(/^ /, "")).split(".");
-    let finalStr = nums[0] + (nums[1] ? "." + (nums[1]).substring(0, 2) : "") + "$"
-
-    if (finalStr[0] == ",") finalStr = finalStr.replace(/^,/, '')
-    
-    return finalStr
-}
-
-function dateTimeFormat(params) {
-    let dateTimeStrs = params.split("T")
-    let date = `${dateTimeStrs[0].split("-")[2]} ${months[dateTimeStrs[0].split("-")[1]]}`
-    let time = `${dateTimeStrs[1].split(":")[0]}:${dateTimeStrs[1].split(":")[1]}` 
-
-    return `${date} ${time}`
-}
-
-function dataSet(params) {
-	for (let i = 0; i < params.length; i++) {
-		const el = params[i];
-
-		chartData.push({
-			t: el.time,
-			o: el.open,
-			h: el.high,
-			l: el.low,
-			c: el.close,
-		})
-		
-	}
-}
-
-function setPoints(params) {
-	for (let i = 0; i < params.length; i++) {
-		const elem = params[i];
-		
-		if (elem.entry_action !== null && elem.exit_action !== null) {
-			pointData.push({
-				code: elem["code"],
-				entry_price: elem["entryPrice"],
-				entry_action: elem["entryAction"],
-				entry_date: elem["entryDate"],
-				entry_date_format: Date.parse(elem["entryDate"]),
-				exit_price: elem["exitPrice"],
-				exit_action: elem["exitAction"]
-			})
-		}
-	}
-}
-
-return chartNode.drawChart(chartJsOptions).then(() => {
-    return chartNode.getImageBuffer('image/png');
-}).then(buffer => {
-    Array.isArray(buffer)
-    return chartNode.getImageStream('image/png');
-}).then(streamResult => {
-    streamResult.stream
-    streamResult.length
-    return chartNode.writeImageToFile('image/png', './testimage.png');
-}).then(() => { });
+generateImage(robot, position, candles)
